64008d4e0ba167d1f1047ff16387fefde8e3dc08
Shiraz Kittelberger-Buehlbecker <buehlbeckershiraz@gmail.com>
2018-06-19 18:55:59 +0200

    routes and navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,7 @@ class PagesController < ApplicationController
 
   def about
   end 
+
+  def dashboard
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/contact'
-  get 'pages/about'
   devise_for :users
   root to: 'pages#home'
+
+  resources :posts do 
+    resources :comments
+  end  
+
+  get '/contact', to: "pages#contact"
+  get '/about', to: "pages#about"
+  get '/dashboard', to: "pages#dashboard"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
