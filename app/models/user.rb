719dd299c4359377cefdae547bcfc56c0c18c018
Shiraz Kittelberger-Buehlbecker <buehlbeckershiraz@gmail.com>
2018-06-29 15:36:30 +0200

    seeded database (still need to add elements to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts, dependent: :destroy
+
+  validates :email, presence: true, allow_blank: false
+  validates :encrypted_password, presence: true, allow_blank: false
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+require "yaml"
+
+puts "Cleaning database..."
+Post.destroy_all
+Comment.destroy_all
+User.destroy_all
+
+# Users
+puts 'Creating users...'
+file = "db/users.yml"
+users = YAML.load(open(file).read)
+
+users = users["users"].each do |user|
+  u = User.new(user)
+  u.save!
+end
+puts "#{User.count} users have been created"
+
+# Posts
+puts 'Creating posts...'
+file = "db/posts.yml"
+posts = YAML.load(open(file).read)
+
+posts["posts"].each do |post|
+  p = Post.new(post)
+  p.user = User.first
+  p.save!
+
+  if post.key?('comments')
+    post["comments"].each do |comment|
+    c = Comment.new(comment)
+    c.post = p
+    c.save!
+    end
+  end
+end
+puts "#{Post.count} posts have been created"
+
+# Comments
+# puts 'Creating comments...'
+# file = "db/comments.yml"
+# comments = YAML.load(open(file).read)
+
+# comments["comments"].each do |comment|
+#   c = Comment.new(comment)
+#   c.post = p
+#   c.save!
+# end
+# puts "#{Comment.count} comments have been created"
