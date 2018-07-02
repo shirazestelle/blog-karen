3397c1a72fd210922fd4c60f35d985d39a967205
Shiraz Kittelberger-Buehlbecker <buehlbeckershiraz@gmail.com>
2018-07-02 13:58:51 +0200

    main validations done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :category, presence: true, inclusion: { in: ["Travel", "Food", "Style", "Love", "Nightlife"] }
+  validates :status, presence: true, inclusion: { in: ["Draft", "Posted"] }
+  validates :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
 
   validates :email, presence: true, allow_blank: false
   validates :encrypted_password, presence: true, allow_blank: false
+
+  validates :first_name, presence: true, uniqueness: true, allow_blank: false
+  validates :last_name, presence: true, uniqueness: true, allow_blank: false
 end
